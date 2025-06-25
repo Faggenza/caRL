10acd046c394d73ff50231bee25e84c1ab14541d
Faggenza <ik3cco@gmail.com>
2025-06-25 15:35:02 +0200

    init gym

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import gymnasium as gym
 
-env = gym.make("CarRacing-v3", render_mode="rgb_array", lap_complete_percent=0.95, domain_randomize=False, continuous=True)
-env.reset()  # Reset the environment to start a new episode
-env.step(env.action_space.sample())  # Take a random action to initialize the environment
-env.render()  # Render the environment
-env.close()  # Close the environment when done
+env = gym.make("CarRacing-v3", render_mode="human", lap_complete_percent=0.95, domain_randomize=False, continuous=True)
+# Number of steps you run the agent for 
+num_steps = 1500
+
+obs = env.reset()
+
+for step in range(num_steps):
+    # take random action, but you can also do something more intelligent
+    # action = my_intelligent_agent_fn(obs) 
+    action = env.action_space.sample()
+    
+    # apply the action
+    obs, rew, done, trunc, info  = env.step(action)
+    
+    # Render the env
+    env.render()
+    
+    # If the epsiode is up, then start another one
+    if done:
+        env.reset()
+
+# Close the env
+env.close()

a66b7b23250ebdcdcbf4a97573ad9bbccb3bfe33
Faggenza <ik3cco@gmail.com>
2025-06-25 15:12:39 +0200

    init venv

---
+++ README.md
@@ -0,0 +1,10 @@
+# caRL
+
+## Init
+Ensure to have `Python 3.11` and `conda` installed.
+To initialize the environment run:
+```bash
+conda create --name caRL_env python=3.11 &&
+conda activate caRL_env &&
+conda install --file requirements.txt
+```
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import gymnasium as gym
 
-env = gym.make("CarRacing-v3", render_mode="rgb_array", lap_complete_percent=0.95, domain_randomize=False, continuous=False)
+env = gym.make("CarRacing-v3", render_mode="rgb_array", lap_complete_percent=0.95, domain_randomize=False, continuous=True)
+env.reset()  # Reset the environment to start a new episode
+env.step(env.action_space.sample())  # Take a random action to initialize the environment
+env.render()  # Render the environment
+env.close()  # Close the environment when done
